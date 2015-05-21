6cf7a6eae3e39314f36d1d0991cb9b9647c00b80
Tulen <juaustria@gmail.com>
2015-05-21 15:00:14 -0700

    new files

---
+++ sketchScript2.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+	$('#counter').append('<p> Please select an option. </p>');
+	
+	$('#button_black').on('click', function(event){
+		event.preventDefault();
+		$('.box').remove();
+	 	var input = prompt('Please choose a number between 1 and 64');
+	 	if (input >= 1 && input <= 64){
+	 		var box_size = $('#pad').width()/input - 2;
+	 		
+	 		for (var i = 1; i <= input; i++){
+	 			for (var j = 1; j <= input; j++){
+	 				$('#pad').append('<div class="box"/>');
+	 			}
+	 			$('#pad').append('<div class="row"/>');
+	 		}
+	 		
+	 		$('.box').css('height',box_size);
+	 		$('.box').css('width',box_size);
+	 		
+	 	}
+	 	else {
+	 		alert('Please try again');
+	 	}
+	 	
+	 	$('#counter').html('<p>Grid Size: ' + input + ' x ' + input + '</p>');
+	 	
+	 	$('.box').on('mouseenter', function(event){
+	 		$(this).toggleClass('black');
+	 	});
+	});
+
+// Plan to work on this later //
+	
+	$('#button_random').on('click', function(event){
+		event.preventDefault();
+		$('.box').remove();
+	 	var input = prompt('Please choose a number between 1 and 64');
+	 	if (input >= 1 && input <= 64){
+	 		var box_size = $('#pad').width()/input - 2;
+	 		
+	 		for (var i = 1; i <= input; i++){
+	 			for (var j = 1; j <= input; j++){
+	 				$('#pad').append('<div class="box"/>');
+	 			}
+	 			$('#pad').append('<div class="row"/>');
+	 		}
+	 		
+	 		$('.box').css('height',box_size);
+	 		$('.box').css('width',box_size);
+	 		
+	 	}
+	 	else {
+	 		alert('Please try again');
+	 	}
+	});
+});
